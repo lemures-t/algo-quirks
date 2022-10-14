d59c8592518553fe59258aee05a66819515e44b7
mark <lht225585@alibaba-inc.com>
2022-10-14 21:36:05 +0800

    :art: test husky with eslint and prettier

--- src/collection/monotonicity-stack.ts
+++ src/collection/monotonicity-stack.ts
@@ -1,85 +1,77 @@
-import AlgoQuirks = require('../AlgoQuirks');
-
+import AlgoQuirks = require("../AlgoQuirks");
 
 /**
  * 单调栈
  */
 
-function lastLargeNum (arr: number[]) : number[]{
-    const res = new Array(arr.length);
-    res.fill(-1);
-    res.forEach(()=>{
+function lastLargeNum(arr: number[]): number[] {
+  const res = new Array(arr.length);
+  res.fill(-1);
 
-    })
+  // 这里单调栈是一个二维数组，内部存储的数组是用于合并arr中有重复数字的情况
+  // 如果题目中给到的 arr 没有重复数字，用一位数组构建单调栈即可
+  const stack: number[][] = [];
 
-    // 这里单调栈是一个二维数组，内部存储的数组是用于合并arr中有重复数字的情况
-    // 如果题目中给到的 arr 没有重复数字，用一位数组构建单调栈即可
-    const stack : number[][]= [];
-
-    for (let i = 0; i < arr.length; i++){
-        const num = arr[i];
-        while(stack.length){
-            const targetArr = stack[stack.length -1];
-            const targetIndex = targetArr[0];
-            const targetNum = arr[targetIndex];
-            if (num > targetNum){
-                stack.pop();
-                for (let j = 0; j < targetArr.length; j++){
-                    res[targetArr[j]] = num;
-                }
-            } else if (num === targetNum){
-                targetArr.push(i);
-                break;
-            } else {                
-                break;
-            }
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    while (stack.length) {
+      const targetArr = stack[stack.length - 1];
+      const targetIndex = targetArr[0];
+      const targetNum = arr[targetIndex];
+      if (num > targetNum) {
+        stack.pop();
+        for (let j = 0; j < targetArr.length; j++) {
+          res[targetArr[j]] = num;
         }
-        stack.push([i])
+      } else if (num === targetNum) {
+        targetArr.push(i);
+        break;
+      } else {
+        break;
+      }
     }
-    while(stack.length){
-        const topArr = stack.pop();
-        res[topArr![0]] = -1;
-    }
-    return res;
-
+    stack.push([i]);
+  }
+  while (stack.length) {
+    const topArr = stack.pop();
+    res[topArr![0]] = -1;
+  }
+  return res;
 }
 class Quirks extends AlgoQuirks<[number[]]> {
-    
-	public ingredientMaker(){
-		const len = parseInt((Math.random() * Math.pow(10, 3)).toString(), 10);
-		const arr : number[]= new Array(len);
-		for (let i = 0 ; i < len; i++){
-				arr[i] = parseInt((Math.random() * Math.pow(10, 3)).toString(), 10)
-		}
-		return [arr] as [number[]]
-	}
+  public ingredientMaker() {
+    const len = parseInt((Math.random() * Math.pow(10, 3)).toString(), 10);
+    const arr: number[] = new Array(len);
+    for (let i = 0; i < len; i++) {
+      arr[i] = parseInt((Math.random() * Math.pow(10, 3)).toString(), 10);
+    }
+    return [arr] as [number[]];
+  }
 
-	public main(ingredient : number[]){
-		return lastLargeNum(ingredient)
-	}
+  public main(ingredient: number[]) {
+    return lastLargeNum(ingredient);
+  }
 
-	public reference(ingredient: number[]){
-		const res : number[] = []
-		for (let i = 0; i < ingredient.length; i++){
-				const target = ingredient[i];
-				for (let j = i+1; j < ingredient.length; j++){
-						const num = ingredient[j];
-						if (num > target){
-								res.push(num)
-								break;
-						}
-				}
-				if (res.length - 1 !== i){
-						res.push(-1);
-				}
-		}
-		return res;
-	}
-
+  public reference(ingredient: number[]) {
+    const res: number[] = [];
+    for (let i = 0; i < ingredient.length; i++) {
+      const target = ingredient[i];
+      for (let j = i + 1; j < ingredient.length; j++) {
+        const num = ingredient[j];
+        if (num > target) {
+          res.push(num);
+          break;
+        }
+      }
+      if (res.length - 1 !== i) {
+        res.push(-1);
+      }
+    }
+    return res;
+  }
 }
 
 const quirks = new Quirks();
 
-
-quirks.run()
+quirks.run(10);
 quirks.print();
