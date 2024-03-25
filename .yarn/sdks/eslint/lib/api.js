1aec87c6ed1b16262f4e20b8e6952b44478e90b0
mark <lht225585@alibaba-inc.com>
2024-03-25 20:13:33 +0800

    chore: yarn dlx @yarnpkg/sdks vscode

--- .yarn/sdks/eslint/lib/api.js
+++ .yarn/sdks/eslint/lib/api.js
@@ -11,10 +11,10 @@ const absRequire = createRequire(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require typescript
+    // Setup the environment to be able to require eslint
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real typescript your application uses
-module.exports = absRequire(`typescript`);
+// Defer to the real eslint your application uses
+module.exports = absRequire(`eslint`);
